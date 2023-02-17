d2036a7d9804bc8deb2c45308f71a9d9ddb581d7
Adrian <xadriantristanx@gmail.com>
2023-02-17 18:33:00 +0100

    Se añade Pestaña AñadirContactos

---
+++ src/MainFrame.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class MainFrame {
+    private JPanel ContactFrame;
+    private JLabel FirstNameLabel;
+    private JTextField FirstNameField;
+    private JLabel LastNameLabel;
+    private JTextField LastNameField;
+    private JButton SaveChangesBtt;
+    private JButton exitButton;
+    private JLabel AddImgLabel;
+    private JLabel PhoneNumberLabel;
+    private JTextField PhoneNumberField;
+    private JButton AddImgButton;
+}
